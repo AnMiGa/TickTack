06c284e69db3381304767b8b84869e0d1260cf87
Angela Gaugler <147183040+AnMiGa@users.noreply.github.com>
2024-05-14 16:35:47 +0200

    Delete angular-frontend/src/app/api directory

--- angular-frontend/src/app/api/api/api.ts
+++
@@ -1,5 +0,0 @@
-export * from './default.service';
-import { DefaultService } from './default.service';
-export * from './timesheet.service';
-import { TimesheetService } from './timesheet.service';
-export const APIS = [DefaultService, TimesheetService];
